23b55fc3ae228dc5e1b9a4d93088cbb3bdecd49e
hai.bui <hai.bui@s3corp.com.vn>
2021-04-15 09:57:20 +0700

    format code

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -20,24 +20,24 @@ class Login extends React.Component {
         if (this.isFormValid(this.state)) {
             this.setState({ errors: [], loading: true });
             firebase
-            .auth()
-            .signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then(signedInUser => {
-                console.log(signedInUser);
-                console.log('Signed');
-                this.setState({loading: false});
-            })
-            .catch(err => {
-                console.log(err);
-                this.setState({
-                    errors: this.state.errors.concat(err),
-                    loading: false
+                .auth()
+                .signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(signedInUser => {
+                    console.log(signedInUser);
+                    console.log('Signed');
+                    this.setState({ loading: false });
+                })
+                .catch(err => {
+                    console.log(err);
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                        loading: false
+                    });
                 });
-           });
         }
     };
 
-    isFormValid = ({email, password}) => email && password;
+    isFormValid = ({ email, password }) => email && password;
 
     handleInputError = (errors, inputName) => {
         return errors.some(error => error.message.toLowerCase().includes(inputName)) ? 'error' : ''
